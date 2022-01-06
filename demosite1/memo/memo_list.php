41b68d3ecf47cc47a4ca1304b92a17078236b597
ImNotAntMan <ikarusppong@gmail.com>
2022-01-06 12:11:35 +0900

    Update memo_list.php

--- demosite1/memo/memo_list.php
+++ demosite1/memo/memo_list.php
@@ -26,6 +26,40 @@ if (!$chk_login) {  // 로그인 상태가 아니라면
     <div class="loginlink"><?=$_SESSION['username']?><button><a href="./membership/user_logout.php">logout</a></button>
 </div>
 </header>
+<?php
+// ===========================================
+  // 여기부터 pagination용 추가
+  // 1. 페이지를 $_GET을 이용하여 전달 받는다. 없으면 현재 $page = 1이다.
+  if(isset($_GET['page_no']) && $_GET['page_no']!="") {
+    $page_no = $_GET['page_no'];
+  } else {
+    $page_no = 1;
+  }
+
+  // 2. 페이지당 보여줄 리스트 갯수값을 정한다.
+  $total_records_per_page = 9;
+
+  // 3. OFFSET을 계산하고 앞/뒤 페이지 등의 변수를 설정한다.
+  $offset = ($page_no - 1) * $total_records_per_page;
+  $previous_page = $page_no - 1;
+  $next_page = $page_no + 1;
+  $adjacents = 2;
+
+  // 4. 전체 페이지 수를 계산한다.
+  $sql = "SELECT COUNT(*) AS total_records FROM memo";
+  $resultset = $conn->query($sql);
+  $result = mysqli_fetch_array($resultset);
+  $total_records = $result['total_records'];
+  $total_no_of_pages = ceil($total_records / $total_records_per_page);
+  $second_last = $total_no_of_pages - 1;
+  // 여기까지 pagination용 추가
+  //=================================================
+  // 다음은 pagination을 위해 기존 코드 수정
+  // $sql = "SELECT * FROM memo";
+  $sql = "SELECT * FROM memo LIMIT ".$offset.", ".$total_records_per_page;
+  $resultset = $conn->query($sql);
+
+?>
     <div>
     <h1>메모 리스트</h1>
     <table border=1>
@@ -33,7 +67,8 @@ if (!$chk_login) {  // 로그인 상태가 아니라면
         <th>제목</th><th>생성일<br>최종수정일</th><th>사용자이름</th><th></th><th></th>
         </tr>
         <?php
-           $sql = "SELECT * FROM memo where userid=".$userid." order by registdate desc";
+           $sql = "SELECT * FROM memo where userid=".$userid." order by registdate desc limit                                                                       ".$offset.", ".$total_records_per_page;
+           echo $sql;
            //$sql = "SELECT memoupdate.subject, toymemoupdate.contents, toymemoupdate.modifydate, toymemoupdate.modify, toymemo.registdate FROM toymemo INNER JOIN toymemoupdate ON toymemo.memoid = toymemoupdate.memoid WHERE toymemoupdate.memoid=".$memoid." ORDER BY modifydate DESC;" ;
            $resultset = $conn->query($sql);
 
@@ -55,6 +90,7 @@ if (!$chk_login) {  // 로그인 상태가 아니라면
               <label for="modifydate"><b>수정일: <?=$modifydate?></b></label><br></label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             
           </div>
+
           <a href="memo_writeForm.php" color="blue">새로운 메모 작성</a>
                 <a href="/index.php">처음으로</a>
             </div>
@@ -89,6 +125,39 @@ if (!$chk_login) {  // 로그인 상태가 아니라면
         }
         ?>
     </table>
+    <ul class="pagination">
+  <?php if($page_no > 1){
+  echo "<li><a href='?page_no=1'>First Page</a></li>";
+  } ?>
+      
+  <li <?php if($page_no <= 1){ echo "class='disabled'"; } ?>>
+  <a <?php if($page_no > 1){
+  echo "href='?page_no=$previous_page'";
+  } ?>>Previous</a>
+  </li>
+<?php
+  
+	for ($counter = 1; $counter <= $total_no_of_pages; $counter++){
+	if ($counter == $page_no) {
+	echo "<li class='active'><a>$counter</a></li>";	
+	        }else{
+        echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+                }
+        }
+?>
+
+  <li <?php if($page_no >= $total_no_of_pages){
+  echo "class='disabled'";
+  } ?>>
+  <a <?php if($page_no < $total_no_of_pages) {
+  echo "href='?page_no=$next_page'";
+  } ?>>Next</a>
+  </li>
+
+  <?php if($page_no < $total_no_of_pages){
+  echo "<li><a href='?page_no=$total_no_of_pages'>Last &rsaquo;&rsaquo;</a></li>";
+  } ?>
+  </ul>
     <table>
         <tr>
             <td>
